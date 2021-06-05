5ef5b6b0642e7e3e4985bddf6a3ada571e2ff166
yuniel <yunielrc87@gmail.com>
2021-06-05 10:29:34 -0400

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,27 @@
 module.exports = {
   testEnvironment: 'node',
+  // The bail config option can be used here to have Jest stop running tests after
+  // the first failure.
+  bail: false,
+  // Indicates whether each individual test should be reported during the run.
+  verbose: false,
+  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
   // testTimeout: 5000,
-  verbose: true,
   // forceExit: true,
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-  ],
+  // If the test path matches any of the patterns, it will be skipped.
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  // If the file path matches any of the patterns, coverage information will be skipped.
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
   // collectCoverageFrom: [
   //   'src/**/*.js',
   // ],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 100,
-  //     functions: 100,
-  //     lines: 100,
-  //     statements: 100,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 };
